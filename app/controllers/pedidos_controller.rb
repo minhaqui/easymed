0736b18965914b5a191bafb4fd39f4487bbdfa5a
Bruno Minhaqui <minhaqui@localhost.localdomain>
2016-07-03 17:39:29 -0300

    remove verify autenticity token

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -1,4 +1,5 @@
 class PedidosController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_pedido, only: [:show, :edit, :update, :destroy]
 
   # GET /pedidos
